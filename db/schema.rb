e6d66fbb3e7d78b44f94b62d9c89e73ca64a9a85
raymond ferranti <ray@raymonds-MacBook-Pro.local>
2015-03-28 21:25:10 -0400

    Added password to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328220832) do
+ActiveRecord::Schema.define(version: 20150329010857) do
 
   create_table "events", force: :cascade do |t|
     t.string   "event_title"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(version: 20150328220832) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "email"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-  	@user = User.new(first_name: "Example", last_name: "User", email: "some@email.com")
+  	@user = User.new(first_name: "Example", last_name: "User", email: "some@email.com",
+                      password: "somePass", password_confirmation: "somePass")
   end
 
   test "should be valid" do
@@ -25,4 +26,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "password should have minimum length of 6" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end

3f0b344c9c2f228b70eac927d0e3a7b730373343
Slavisa Djukic <ssdjuka@gmail.com>
2015-03-31 23:35:28 -0400

    Started work on tickets.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def show
+  	@user = User.find(1)
+  	@events = Event.where("event_date > Time.now")
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(first_name: "First", last_name: "User", email: "first@example.com")
+User.create(first_name: "Second", last_name: "User", email: "second@example.com")
+User.create(first_name: "Third", last_name: "User", email: "third@example.com")
+
+Event.create(title: "Second event", place: "Second place", 
+	         description: "Second event to acquaint children with technology",
+	         event_date: Time.new(2015, 5, 14, 13, 00),
+	         attendance: 25)

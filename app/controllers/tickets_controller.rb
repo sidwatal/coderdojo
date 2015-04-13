b128bbfd0ec44f83ccc142dd8e614c48a9e740d4
rickyc <rickyc.us@gmail.com>
2015-04-13 08:21:52 -0400

    just cleaning up the spacing.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,15 +1,15 @@
 class TicketsController < ApplicationController
- 
+
   def create
     @current_user = User.find(params[:ticket][:user_id])
     event = Event.find(params[:ticket][:event_id])
-    ticket = Ticket.new(user: @current_user, 
-                        event: event, 
+    ticket = Ticket.new(user: @current_user,
+                        event: event,
                         ticket_type: params[:ticket][:user_role],
-                        activity: params[:ticket][:activity], 
-    	                  number_of_children: params[:ticket][:number_of_children])
-   
-    unless @current_user.has_ticket?(params[:ticket][:event_id])  
+                        activity: params[:ticket][:activity],
+                        number_of_children: params[:ticket][:number_of_children])
+
+    unless @current_user.has_ticket?(params[:ticket][:event_id])
       if ticket.save()
         redirect_to @current_user, notice: "Ticket created."
       else
@@ -37,13 +37,13 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-  	params.require(:ticket)
-          .permit( :user_id, 
-                   :event_id, 
-                   :user_role,
-                   :activity,
-                   :ticket_type, 
-                   :number_of_children)
+    params.require(:ticket)
+    .permit( :user_id,
+            :event_id,
+            :user_role,
+            :activity,
+            :ticket_type,
+            :number_of_children)
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,33 +1,31 @@
 class Event < ActiveRecord::Base
-	
-	has_many :ticket
-	has_many :users, through: :tickets
 
-	validates :title, presence: true
-	validates :place, presence: true
-	validates :description, presence: true
-	validates :event_date, presence: true
-	validates :attendance, presence: true, numericality: { only_integer: true, greater_than: 0 }
-	validates :activities, presence: true
-	
-	# return current events
-	def self.current
+  has_many :ticket
+  has_many :users, through: :tickets
+
+  validates :title, presence: true
+  validates :place, presence: true
+  validates :description, presence: true
+  validates :event_date, presence: true
+  validates :attendance, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :activities, presence: true
+
+  # return current events
+  def self.current
     Event.where("event_date >?", Time.now)
-	end
+  end
 
   # number of seconds in a day
   @@day = 86400
   # will return first event; not handling multiple events in a single day
   def self.todays_event
-  	Event.where("event_date between ? and ?", 
-  		          Time.now - @@day,
-  		          Time.now + @@day).first
+    Event.where("event_date between ? and ?", Time.now - @@day, Time.now + @@day).first
   end
 
-	def current_attendance
-		all_tickets = Ticket.where("event_id =?", self.id)
+  def current_attendance
+    all_tickets = Ticket.where("event_id =?", self.id)
     all_tickets.inject(0){|sum, t| sum + t.number_of_children}
-	end
+  end
 
   # list all users registered for the event
   def list_of_users
